33ec445a4d28c9b2b0d202bd074ef7612fe5e8fd
c4rlo5Riv3ra <jcriveraguadalupe@gmail.com>
2025-08-06 23:29:22 -0500

    add: login and logout

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
 
 # Create your views here.
 def home(request) :
@@ -20,10 +21,39 @@ def signup(request) :
       try :
         user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
         user.save()
-        return HttpResponse('User created successfully')
-      except :
-        return HttpResponse('Username already exists')
+        login(request, user)
+        return redirect('tasks')
+      except IntegrityError:
+        return render(request, 'signup.html', {
+          'form' : UserCreationForm,
+          'error' : 'username already exists'
+        })
     
-    return HttpResponse('password do not match')
+    return render(request, 'signup.html', {
+      'form' : UserCreationForm,
+      'error' : 'passwords do not match'
+    })
 
-  
+def tasks(request):
+  return render(request, 'tasks.html')
+
+def signout(request):
+  logout(request)
+  return redirect('home')
+
+def signin(request):
+  if request.method == 'GET':
+    return render(request, 'signin.html', {
+      'form' : AuthenticationForm
+    })
+  else :
+    user = authenticate(username=request.POST['username'], password=request.POST['password'])
+    
+    if user is None :
+      return render(request, 'signin.html', {
+        'form' : AuthenticationForm,
+        'error' : 'username or password is incorrect'
+      })
+    else :
+      login(request, user)
+      return redirect('tasks')
